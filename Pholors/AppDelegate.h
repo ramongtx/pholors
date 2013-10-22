394b8772b2cdbef769d1f913918153cf122ca78d
ramongtx <ramon@Ramons-MacBook-Pro.local>
2013-10-22 16:16:22 -0200

    Initial Commit

---
+++ Pholors/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pholors
+//
+//  Created by Ramon Carvalho Maciel on 10/22/13.
+//  Copyright (c) 2013 Rock Bottom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pholors/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pholors
+//
+//  Created by Ramon Carvalho Maciel on 10/22/13.
+//  Copyright (c) 2013 Rock Bottom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
