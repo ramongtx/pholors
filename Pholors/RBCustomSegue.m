454f12672fc7e37d4ddfcc4116915d09248659ad
rpadilha <rafaspadilha@gmail.com>
2013-10-23 15:30:48 -0200

    Added RBCustomSegue

---
+++ Pholors/RBCustomSegue.m
@@ -0,0 +1,24 @@
+//
+//  RBCustomSegue.m
+//  Pholors
+//
+//  Created by Rafael Padilha on 23/10/13.
+//  Copyright (c) 2013 Rock Bottom. All rights reserved.
+//
+
+#import "RBCustomSegue.h"
+
+@implementation RBCustomSegue
+
+- (void) perform {
+    UIViewController *src = (UIViewController *) self.sourceViewController;
+    UIViewController *dst = (UIViewController *) self.destinationViewController;
+    [UIView transitionWithView:src.navigationController.view duration:0.2
+                       options:UIViewAnimationOptionTransitionFlipFromLeft
+                    animations:^{
+                        [src.navigationController pushViewController:dst animated:NO];
+                    }
+                    completion:NULL];
+}
+
+@end
