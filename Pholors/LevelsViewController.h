5cfbbc0b23d7f5dc26d206c3600071bd509ee4bc
Felix Dumit <felix.dumit@gmail.com>
2013-10-23 15:29:01 -0200

    started LevelsViewController

---
+++ Pholors/LevelsViewController.h
@@ -0,0 +1,14 @@
+//
+//  LevelsViewController.h
+//  Pholors
+//
+//  Created by Felix Dumit on 10/23/13.
+//  Copyright (c) 2013 Rock Bottom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RBGame.h"
+
+@interface LevelsViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
