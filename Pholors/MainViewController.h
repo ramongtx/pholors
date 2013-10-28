1d173d16b281691154466b122ac86b205c391eaa
ramongtx <ramongtx@gmail.com>
2013-10-28 11:37:52 -0200

    New screen

---
+++ Pholors/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Pholors
+//
+//  Created by Ramon Carvalho Maciel on 10/28/13.
+//  Copyright (c) 2013 Rock Bottom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
