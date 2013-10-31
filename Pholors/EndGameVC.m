6427b7608bf8fdee47feb3d8453e0a6f49305db6
Felix Dumit <felix.dumit@gmail.com>
2013-10-31 15:52:41 -0200

    change timeattack back to 80s

--- Pholors/EndGameVC.m
+++ Pholors/EndGameVC.m
@@ -28,7 +28,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    NSString* str = [NSString stringWithFormat:@"You scored:%d points", self.points];
+    NSString* str = [NSString stringWithFormat:@"You scored: %d points", self.points];
     if(self.highscore)
         str = [str stringByAppendingString:@"(HIGH SCORE)"];
     self.scoreLabel.text = str;
